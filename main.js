1ff13cee1e97c5167c8fcbe32b8c7200794b5d24
Bozhuk <bozhukihor1994@gmail.com>
2022-01-27 00:16:18 +0200

    add some fixes

--- main.js
+++ main.js
@@ -1,77 +1,86 @@
 const content = document.querySelector(".content__inner");
-const preview = document.querySelector(".form__image")
-const formImageDescr = document.querySelector(".form__image-descr")
-const nextBtn = document.getElementById("next-btn");
+const preview = document.querySelector(".form__image");
+const formImageDescr = document.querySelector(".form__image-descr");
 const form = document.getElementById("form");
-const formImage = document.getElementById("form-image")
-const formTitle = document.getElementById("form-title")
-const formText = document.getElementById("form-text")
-const formLink = document.getElementById("form-link")
-const fileReader = new FileReader()
+const formImage = document.getElementById("form-image");
+const formTitle = document.getElementById("form-title");
+const formText = document.getElementById("form-text");
+const formLink = document.getElementById("form-link");
+const nextBtn = document.getElementById("next-btn");
+const fileReader = new FileReader();
+const elemPerPage = 9;
+let currentPage = 1;
 
-formTitle.addEventListener("input", () => {
-    formTitle.value = formTitle.value.replace(/[a-z0-9]/gi, '');
-    if (formTitle.value.length >= 20) {
-        alert("Длина заголовка не может быть больше 20 симвловов")
-        formTitle.value = formTitle.value.substr(0, 20)
-        formTitle.style.border = "red 2px solid"
+const validateInput = (elem, limit) => {
+    elem.value = elem.value.replace(/[a-z0-9]/gi, '');
+    if (elem.value.length >= limit) {
+        document.querySelector(`.${elem.id}-invalid`).style.display = "block"
+        elem.value = elem.value.substr(0, limit)
     }
-    else formTitle.style.border = "none"
-})
-formText.addEventListener("input", () => {
-    formText.value = formText.value.replace(/[a-z0-9]/gi, '');
-    if (formText.value.length >= 250) {
-        formText.value = formText.value.substr(0, 250)
-        alert("Длина описания не должна превышать 250 симвловов")
-        formText.style.border = "red 2px solid"
-    }
-    else formText.style.border = "none"
-})
-formImage.addEventListener('change', () => {
-    if (formImage.files[0]) {
+    else document.querySelector(`.${elem.id}-invalid`).style.display = "none"
+}
+const previewImage = (elem) => {
+    fileReader.readAsDataURL(elem.files[0])
+    if (elem.files[0]) {
         fileReader.onload = (event) => {
             preview.style.backgroundImage = 'url(' + event.target.result + ')'
         }
-        fileReader.readAsDataURL(formImage.files[0])
         formImageDescr.style.display = "none"
     }
-})
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    if (formImage.value && formTitle.value && formText.value && formLink.value) {
-        const productBox = `<div class="product__box">
+}
+const createBox = () => {
+    const productBox = `<div class="product__box">
                     <a class="product__link" href="${formLink.value}">
-                        <img class="product__image"  alt="some-product">
+                        <img class="product__image" alt="some-product">
                         <h3 class="product__title">${formTitle.value}</h3>
                         <p class="product__text">${formText.value}</p>
                     </a>
                     </div>`
-        content.insertAdjacentHTML("beforeend", productBox)
-        fileReader.readAsDataURL(formImage.files[0])
-        fileReader.onload = (event) => {
-            let images = document.querySelectorAll(".product__image");
-            images[images.length - 1].setAttribute("src", event.target.result)
-        }
-        form.reset()
-        preview.style.backgroundImage = null
-        formImageDescr.style.display = "block"
+    content.insertAdjacentHTML("beforeend", productBox)
+}
+const addImageToBox = () => {
+    fileReader.readAsDataURL(formImage.files[0])
+    fileReader.onload = (e) => {
+        let images = document.querySelectorAll(".product__image");
+        images[images.length - 1].setAttribute("src", e.target.result)
     }
-})
-content.addEventListener('DOMNodeInserted', () => {
-    if (content.children.length > 9) {
-        nextBtn.style.display = "block"
-        for (let i = 9; i < content.children.length; i++) {
+    preview.style.backgroundImage = null
+    formImageDescr.style.display = "block"
+}
+const submitHandler = (e) => {
+    e.preventDefault()
+    const data = [
+        formImage.value,
+        formTitle.value,
+        formText.value,
+        formLink.value
+    ]
+    if (data.length == 4) {
+        createBox()
+        addImageToBox()
+        form.reset()
+    }
+}
+const hideElements = () => {
+    if (content.children.length > elemPerPage * currentPage) {
+        nextBtn.style.display = "block";
+        for (let i = elemPerPage * currentPage; i < content.children.length; i++) {
             content.children[i].style.display = "none"
         }
-        nextBtn.addEventListener("click", () => {
-            for (let i = 9; i < content.children.length; i++) {
-                content.children[i].style.display = "flex"
-            }
-            nextBtn.style.display = "none"
-        })
     }
-
-})
-
-
-
+}
+const showMoreElements = () => {
+    nextBtn.style.display = "none"
+    for (let i = elemPerPage * currentPage; i < elemPerPage * (currentPage + 1); i++) {
+        if (content.children[i]) content.children[i].style.display = "flex"
+        else return
+    }
+    currentPage += 1
+    if (content.children.length > elemPerPage * currentPage) nextBtn.style.display = "block"
+}
+formTitle.addEventListener("input", () => validateInput(formTitle, 20));
+formText.addEventListener("input", () => validateInput(formText, 250));
+formImage.addEventListener('change', () => previewImage(formImage));
+form.addEventListener('submit', () => submitHandler(event));
+content.addEventListener('DOMNodeInserted', () => hideElements());
+nextBtn.addEventListener("click", () => showMoreElements());
